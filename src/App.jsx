f8b41380a7d019412eaa79bf3f6b02ab700f7b17
Maduwanthi Lakshika <145977664+maduwanthii@users.noreply.github.com>
2025-03-15 13:38:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./components/Sidebar/sidebar"; // Assuming Sidebar is in the Sidebar folder
+import Main from "./components/Main/MAin";         // Assuming Main is in the Main folder
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+       <Main /> 
+    </>
+  );
+};
+
+export default App;
